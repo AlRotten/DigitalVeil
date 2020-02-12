3aec1aa75a5716de3e432f07738685652ec75f6e
Al <alrodesis@gmail.com>
2020-02-12 07:36:40 -0800

    First Commit

---
+++ DigitalVeil/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DigitalVeil
+//
+//  Created by Al Rodríguez on 10/02/2020.
+//  Copyright © 2020 AlSoft - Al Rodríguez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ DigitalVeil/launchController.swift
@@ -0,0 +1,9 @@
+//
+//  launchController.swift
+//  DigitalVeil
+//
+//  Created by Al Rodríguez on 11/02/2020.
+//  Copyright © 2020 Al Rodríguez. All rights reserved.
+//
+
+import Foundation
